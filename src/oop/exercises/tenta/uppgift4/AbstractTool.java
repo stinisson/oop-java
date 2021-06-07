08c4814df839ec1e8c0af2c27b1a6c5a15339453
Stin <krikarlss@gmail.com>
2021-06-07 15:39:15 +0200

    Exercise 4

---
+++ src/oop/exercises/tenta/uppgift4/AbstractTool.java
@@ -0,0 +1,19 @@
+public abstract class AbstractTool implements Tool { 
+
+    public DrawCanvas getCanvas() {
+        return canvas;
+    }
+
+    public String getName() {
+        return name; 
+    }
+
+    public AbstractTool(DrawCanvas canvas, String name) {
+        this.canvas = canvas;
+        this.name = name; 
+    }
+
+    private DrawCanvas canvas; 
+    private String name;
+
+}
---
+++ src/oop/exercises/tenta/uppgift4/Tool.java
@@ -0,0 +1,11 @@
+
+import java.awt.*;
+
+public interface Tool { 
+
+    public String getName(); 
+    public void startShape(Point p); 
+    public void addPointToShape(Point p);
+    public void endShape(Point p); 
+
+}
